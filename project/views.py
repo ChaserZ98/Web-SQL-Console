a0b9ece8c8952daa6c37b61afd32e52652c6b0a0
Feiyu Zheng <feiyuzheng98@gmail.com>
2021-11-30 23:43:26 -0500

    update

--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
 import traceback
 
+import sqlparse
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.http import JsonResponse
@@ -8,9 +9,11 @@ import time
 from django.db import connections
 import re
 from project import mongoClient
+from datetime import datetime
 
 # def index(request):
 #     return HttpResponse('project index page')
+
 
 # connect to mysql by default when the project index page is requested for the first time
 def index(request):
@@ -49,6 +52,9 @@ def connectToDB(request):
             result = cursor.fetchone()[0]
         elif databaseType == 'mongodb':
             result = db.name
+            # temp = eval("db.instacart_fact_table.find().limit(1)")
+            # for doc in temp:
+            #     print(doc)
     except Exception as e:
         responseStatus = 1
         traceback.print_exc()
@@ -61,15 +67,16 @@ def checkQuery(query, row, rowPerPage):
     # since show, describe doesn't support limit, then only when using select command will we append limit and offset
     if str(query[:6]).lower() == 'select':
         # In redshift (postgreSQL), top and limit is not allowed to use together.
-        top = re.search('\s+top\s+(\d+)', query, re.IGNORECASE)
-        limitOffset = re.search('\s+limit\s+(\d+)\s*,\s+(\d+)', query, re.IGNORECASE)
-        limit = re.search('\s+limit\s+(\d+)', query, re.IGNORECASE)
-        offset = re.search('\s+offset\s+(\d+)', query, re.IGNORECASE)
+        query = query.replace(';', '')
+        top = re.search('\s+top\s+(\d+)', query, flags=re.IGNORECASE)
+        limitOffset = re.search('\s+limit\s+(\d+)\s*,\s+(\d+)', query, flags=re.IGNORECASE)
+        limit = re.search('\s+limit\s+(\d+)', query, flags=re.IGNORECASE)
+        offset = re.search('\s+offset\s+(\d+)', query, flags=re.IGNORECASE)
         if top:
             limitNum = int(top.group(1))
-            query = re.sub('(\s+top\s+\d+)', '', query, re.IGNORECASE)
+            query = re.sub('(\s+top\s+\d+)', '', query, flags=re.IGNORECASE)
             if offset:
-                query = re.sub('(\s+offset\s+\d+)', '', query, re.IGNORECASE)
+                query = re.sub('(\s+offset\s+\d+)', '', query, flags=re.IGNORECASE)
                 offsetNum = int(offset.group(1))
             if rowPerPage + row > limitNum:
                 query += " limit %s offset %s" % (limitNum - row, row if not offset else row + offsetNum)
@@ -78,23 +85,23 @@ def checkQuery(query, row, rowPerPage):
         elif limitOffset:
             offsetNum = int(limitOffset.group(1))
             limitNum = int(limitOffset.group(2))
-            query = re.sub('(\s+limit\s+\d+\s*,\s+\d+)', '', query, re.IGNORECASE)
+            query = re.sub('(\s+limit\s+\d+\s*,\s+\d+)', '', query, flags=re.IGNORECASE)
             if rowPerPage + row > limitNum:
                 query += " limit %s offset %s" % (limitNum - row, row + offsetNum)
             else:
                 query += " limit %s offset %s" % (rowPerPage, row + offsetNum)
         elif limit:
             limitNum = int(limit.group(1))
-            query = re.sub('(\s+limit\s+\d+)', '', query, re.IGNORECASE)
+            query = re.sub('(\s+limit\s+\d+)', '', query, flags=re.IGNORECASE)
             if offset:
-                query = re.sub('(\s+offset\s+\d+)', '', query, re.IGNORECASE)
+                query = re.sub('(\s+offset\s+\d+)', '', query, flags=re.IGNORECASE)
                 offsetNum = int(offset.group(1))
             if rowPerPage + row > limitNum:
                 query += " limit %s offset %s" % (limitNum - row, row if not offset else row + offsetNum)
             else:
                 query += " limit %s offset %s" % (rowPerPage, row if not offset else row + offsetNum)
         elif offset:
-            query = re.sub('(\s+offset\s+\d+)', '', query, re.IGNORECASE)
+            query = re.sub('(\s+offset\s+\d+)', '', query, flags=re.IGNORECASE)
             offsetNum = int(offset.group(1))
             row += offsetNum
             query += " limit %s offset %s" % (rowPerPage, row)
@@ -157,16 +164,22 @@ def ajax(request):
     databaseType = request.POST.get('databaseType')  # mysql/redshift/mongoDB
     currentDatabase = request.POST.get('currentDatabase')  # used database/schema
     query = str(request.POST.get('query'))  # input sql query
-    query = [sql.replace('\n', ' ').strip() for sql in query.split(';') if sql]
+    query = sqlparse.format(query, reindent=True, keyword_case='upper')
+    query = sqlparse.split(query)
+
+    # query = [sql.replace('\n', ' ').strip() for sql in query.split(';') if sql]
     print('Received Query: ', query)
 
     # send data
-    tableQuery = query  # query that generates each table
-    resultAttribute = []  # table attribute
-    queryResult = []  # table data
-    totalRecords = []  # numeric value of number of rows influenced by a sql query
-    influencedRowResult = []  # string value of number of rows influenced by a sql query
-    totalTime = 0  # total time of executing all sql queries
+    tableQuery = query          # query that generates each table
+    displayedQuery = []         # query that will be displayed in result section
+    resultAttribute = []        # table attribute
+    queryResult = []            # table data
+    totalRecords = []           # numeric value of number of rows influenced by a sql query
+    executedTimeStamp = []      # string value of timestamp when each query is executed
+    influencedRowResult = []    # string value of number of rows influenced by a sql query
+    executionTime = []          # string value of execution time of each query
+    totalExecutionTime = 0      # total time of executing all sql queries
     # currentDatabase           # return the current database/schema back in case it changed
 
     cursor = None
@@ -190,11 +203,31 @@ def ajax(request):
             traceback.print_exc()
             return JsonResponse({'responseStatus': responseStatus, 'errorDetails': str(e)})
         endTime = time.time()
-        totalTime += endTime - startTime
+        totalExecutionTime += endTime - startTime
+
+        executionTime.append(str(round(endTime - startTime, 2)) + ' s')
         influencedRow = cursor.rowcount
         totalRecords.append(influencedRow)
-        # DML (INSERT, UPDATE) will use "affected", DQL (SELECT) will use "retrieved"
+        executedTimeStamp.append(datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+
         try:
+            # change the color of keywords and identifier in the query
+            colorQuery = ""
+            keywordColor = "rgb(43, 51, 179)"
+            identifierColor = "rgb(134, 179, 0)"
+            for token in sqlparse.parse(sql)[0].flatten():
+                if token.ttype in sqlparse.tokens.Keyword:
+                    colorQuery += f"<span style='color: {keywordColor}'>{token.value}</span>"
+                elif token.ttype in sqlparse.tokens.Name or token.ttype in sqlparse.tokens.Wildcard:
+                    colorQuery += f"<span style='color: {identifierColor}'>{token.value}</span>"
+                elif token.ttype is sqlparse.tokens.Whitespace:
+                    colorQuery += '&nbsp;'
+                else:
+                    colorQuery += token.value
+            colorQuery = colorQuery.replace("\n", "</br>")
+            displayedQuery.append(colorQuery)
+
+            # DML (INSERT, UPDATE) will use "affected", DQL (SELECT) will use "retrieved"
             if influencedRow == 0 or influencedRow == -1:
                 influencedRowResult.append(None)
             elif influencedRow == 1:
@@ -236,11 +269,12 @@ def ajax(request):
     try:
         for i in range(len(influencedRowResult)):
             print(f'Executed Query: {query[i]}')
-            print(influencedRowResult[i])
+            print(f'Displayed Query: {displayedQuery[i]}')
+            print(f'Influenced Row: {influencedRowResult[i]}')
             print(f'Result Attributes: {resultAttribute[i]}')
             print(f'Query Result: {queryResult[i][:100]}')  # limit query result to 100 rows
             print()
-        print(f'Total Execution Time: {round(totalTime, 2)} s')
+        print(f'Total Execution Time: {round(totalExecutionTime, 2)} s')
     except Exception as e:
         responseStatus = 1
         traceback.print_exc()
@@ -260,12 +294,19 @@ def ajax(request):
 
     return JsonResponse(
         {'tableQuery': tableQuery,
+         'displayedQuery': displayedQuery,
          'totalRecords': totalRecords,
+         'executedTimeStamp': executedTimeStamp,
          'influencedRowResult': influencedRowResult,
          'resultAttribute': resultAttribute,
          'queryResult': queryResult,
-         'executionTime': str(round(totalTime, 2)) + ' s',
+         'executionTime': executionTime,
+         'totalExecutionTime': str(round(totalExecutionTime, 2)) + ' s',
          'currentDatabase': str(currentDatabase)})
+
+
+# def sqlToMongo(query):
+#     if str(query[:6]).lower() == 'select':
 
 
 def testMongo(request):
