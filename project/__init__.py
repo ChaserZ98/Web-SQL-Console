dc21b9cfb14494fa5a7d89a3f21478dd84d27566
Feiyu Zheng <feiyuzheng98@gmail.com>
2021-12-06 05:11:22 -0500

    update

--- project/__init__.py
+++ project/__init__.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 from cs527_web_project.settings import DATABASES
 
 pymysql.install_as_MySQLdb()
-mongoClient = MongoClient(DATABASES["mongodb"]["HOST"], DATABASES["mongodb"]["PORT"])
+mongoClient = MongoClient(DATABASES["mongodb"]["HOST"], DATABASES["mongodb"]["PORT"], connect=False)
